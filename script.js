5f9b62b79824d0cc39c508660f39f57c597f38b6
Muhammed Karim <muhammedkarim@hotmail.co.uk>
2026-03-19 17:29:54 +0000

    2 dhikr times

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ function initPrayerTimes() {
         if (!dhikrData) return;
         document.getElementById("dhikr-morning").textContent = formatTo12Hour(getDisplayTime("morning")) || "-";
         document.getElementById("dhikr-evening").textContent = formatTo12Hour(getDisplayTime("evening")) || "-";
-        document.getElementById("dhikr-night").textContent =  formatTo12Hour(getDisplayTime("night")) || "-";
+        // document.getElementById("dhikr-night").textContent =  formatTo12Hour(getDisplayTime("night")) || "-";
       })
       .catch(err => console.error("Dhikr fetch error:", err));
   }
@@ -493,7 +493,7 @@ function initPrayerTimes() {
   checkMakroohPoster();
   preloadAndCheckPosters();
   checkLiveStatusAndToggleOverlay();
-  pollTaraweehStateAndApply();
+  // pollTaraweehStateAndApply();
   
   setInterval(updateClock, 1000);
   setInterval(loadPrayerTimes, 1000);
@@ -504,5 +504,5 @@ function initPrayerTimes() {
   setInterval(refreshPosters, 600000);
   setInterval(checkLiveStatusAndToggleOverlay, 5000);
   setInterval(checkVersionAndReload, 60000);
-  setInterval(pollTaraweehStateAndApply, 1000);
+  // setInterval(pollTaraweehStateAndApply, 1000);
 }
